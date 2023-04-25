0751b543d5b48302ca56ce1a807febe6a39e9c6f
rdntmsn <rdntmsn@gmail.com>
2023-04-25 22:19:49 +0100

    add table to home page

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from flask import Flask, render_template
 import pandas as pd
 
-
+# create app
 app = Flask(__name__)
 
+# read data file and select columns for view
 stations = pd.read_csv('data_small/stations.txt', skiprows=17)
 stations = stations[['STAID', 'STANAME                                 ']]
 
 
+# define home page
 @app.route('/')
 def home():
 
     return render_template('home.html', data=stations.to_html())
 
 
+# define api url
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
+    # filename with zfill format.
     filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    # parce the date from the data
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    # data_no_missing_value = df.loc[df['   TG'] != -9999]
-    # df["TG0"] = df['   TG'].mask(df['   TG'] == -9999, np.nan)
-    # df["TG"] = df['TG0'] / 10
-    # date = data_no_missing_value["    DATE"]
+    # squeeze temperature according to date
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
 
     return {"station": station,
