64c3424a0ce93489a0303c6ed16d67d25abb9dc6
rdntmsn <rdntmsn@gmail.com>
2023-04-25 18:32:47 +0100

    returned temperature from station and date inputs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -11,7 +13,13 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
+    filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    # data_no_missing_value = df.loc[df['   TG'] != -9999]
+    # df["TG0"] = df['   TG'].mask(df['   TG'] == -9999, np.nan)
+    # df["TG"] = df['TG0'] / 10
+    # date = data_no_missing_value["    DATE"]
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
 
     return {"station": station,
             "date": date,
