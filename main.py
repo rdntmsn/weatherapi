a22a8b94bb489c24a23cd89942dc8f414c68ce89
rdntmsn <rdntmsn@gmail.com>
2023-04-26 08:29:49 +0100

    add url endpoints

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def home():
     return render_template('home.html', data=stations.to_html())
 
 
-# define api url
+# define api url for station and date
 @app.route("/api/v1/<station>/<date>")
-def about(station, date):
+def station_date(station, date):
     # filename with zfill format.
     filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
     # parce the date from the data
@@ -29,6 +29,25 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def all_station_data(station):
+    # filename with zfill format.
+    filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient='records')
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def single_year_data(station, year):
+    # filename with zfill format.
+    filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient='records')
+    return result
 
 
 if __name__ == '__main__':
