ef1cad0b6dec9956be23a8f9f828896989573814
rdntmsn <rdntmsn@gmail.com>
2023-05-03 19:30:40 +0100

    add images and fixed errors and formatting

--- prediction_backend.py
+++ prediction_backend.py
@@ -3,7 +3,7 @@ import requests
 API_KEY = "a8a1faca976a4cb582a834f2325410ec"
 
 
-def get_data(place, days=None, weather_report=None):
+def get_data(place, days=None):
     api_url = f'http://api.openweathermap.org/data/2.5/forecast?' \
               f'q={place}&' \
               f'appid={API_KEY}'
@@ -12,13 +12,8 @@ def get_data(place, days=None, weather_report=None):
     filtered_data = data['list']
     num_of_values = 8*days
     filtered_data = filtered_data[:num_of_values]
-    if weather_report == 'Temperature':
-        filtered_data = [dict["main"]['temp'] for dict in filtered_data]
-
-    if weather_report == 'Sky':
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == '__main__':
-    print(get_data(place='Tokyo', days=3, weather_report='Temperature'))
+    print(get_data(place='Tokyo', days=3))
--- prediction_main.py
+++ prediction_main.py
@@ -2,23 +2,42 @@ import streamlit as st
 import plotly.express as px
 from prediction_backend import get_data
 
+# front end
+# set the title
 title = st.title('Weather Forecast for the Next Days')
 
+# text input for the place
 place = st.text_input("Place:")
+
+# slider to select the number of days
 days = st.slider('Forecast Days: ', min_value=1, max_value=5,
                  help='Select the number of forecast days')
-
+# menu to select either temperature or sky view
 data_to_view = st.selectbox('Select data to view',
                             ('Temperature', 'Sky'))
+# shows the user what will be viewed
+st.subheader(f'{data_to_view} for the next {days} days in {place.title()}')
 
-st.subheader(f'{data_to_view} for the next {days} days in {place}')
+# get data from front end
+if place:
+    try:
+        filtered_data = get_data(place, days)
 
-data = get_data(place, days, data_to_view)
+        # the temperature in chart
+        if data_to_view == 'Temperature':
+            temps = [round(dict["main"]['temp'] / 10, 2) for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temps,
+                             labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-dates = ["4/30", "5/1", "5/2"]
-temps = [30, 23, 21]
-temps = [days * i for i in temps]
-figure = px.line(x=dates, y=temps,
-                 labels={"x": "Dates", "y":"Temperatures (C)"})
-st.plotly_chart(figure)
+        # the sky charted
+        if data_to_view == 'Sky':
+            images = {"Clear": 'sky/clear.png', 'Clouds': 'sky/cloud.png',
+                      'Rain': 'sky/rain.png', 'Snow': 'sky/snow.png'}
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=75)
 
+    except KeyError:
+        st.info('You have entered a city that does not exist. Please enter the correct city name')
